19a50f25d7d782a7c07ec929ce0cd20befa67ff8
Damian Sikora <dsikora85@gmail.com>
2019-08-16 19:20:49 +0200

    Added app logic

--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,40 @@ if ('serviceWorker' in navigator) {
 
 
 console.log(`Hello world!`);
+
+const addBtn = document.querySelector(".frame__button--add--js");
+const removeBtn = document.querySelector(".frame__button--remove--js");
+const glassNum = document.querySelector(".frame_num--js");
+
+let glassAmount = 0;
+const key = new Date().toISOString().slice(0, 10);
+if (localStorage.getItem(key) === null) {
+    localStorage.setItem(key, 0);
+} else {
+    glassAmount = localStorage.getItem(key);
+}
+
+glassNum.innerHTML = glassAmount;
+
+addBtn.addEventListener('click', e => {
+    glassAmount = parseInt(glassNum.innerHTML);
+    if (glassAmount < 9) {
+        glassAmount++;
+        glassNum.innerHTML = glassAmount;
+        storeGlassNum(glassAmount);
+    }
+});
+
+removeBtn.addEventListener('click', e => {
+    glassAmount = parseInt(glassNum.innerHTML);
+    if (glassAmount > 0) {
+        glassAmount--;
+        glassNum.innerHTML = glassAmount;
+        storeGlassNum(glassAmount);
+    }
+});
+
+function storeGlassNum(value) {
+    const key = new Date().toISOString().slice(0, 10);
+    localStorage.setItem(key, value);
+}
